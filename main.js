dfdd0353edfe454086c28b210231bd9f8c9152e0
mikesmall <michael.l.a.small@gmail.com>
2017-07-14 17:51:59 -0400

    Changed some colours; declared 'square' JS variables

--- main.js
+++ main.js
@@ -6,5 +6,15 @@ document.addEventListener('DOMContentLoaded', function() {
     location.reload();
   });
 
+  var squareOne = document.querySelector('.one');
+  var squareTwo = document.querySelector('.two');
+  var squareThree = document.querySelector('.three');
+  var squareFour = document.querySelector('.four');
+  var squareFive = document.querySelector('.five');
+  var squareSix = document.querySelector('.six');
+  var squareSeven = document.querySelector('.seven');
+  var squareEight = document.querySelector('.eight');
+  var squareNine = document.querySelector('.nine');
+
 // end of 'DOMContentLoaded'
 });
