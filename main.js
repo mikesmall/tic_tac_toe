d7a2dbb3f7c3028d730e50fdf753b9e5f0160ad1
mikesmall <michael.l.a.small@gmail.com>
2017-07-14 18:47:27 -0400

    Event listeners for each square

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  // Button-click to reset the board:
   var newGame = document.querySelector('.new_game');
   newGame.addEventListener('click', function() {
     console.log('The button was clicked');
     location.reload();
   });
 
+  // Variables to represent each square:
   var squareOne = document.querySelector('.one');
   var squareTwo = document.querySelector('.two');
   var squareThree = document.querySelector('.three');
@@ -15,6 +17,17 @@ document.addEventListener('DOMContentLoaded', function() {
   var squareSeven = document.querySelector('.seven');
   var squareEight = document.querySelector('.eight');
   var squareNine = document.querySelector('.nine');
+
+  // Event listeners for each square:
+  squareOne.addEventListener('click', clickEvent);
+  squareTwo.addEventListener('click', clickEvent);
+  squareThree.addEventListener('click', clickEvent);
+  squareFour.addEventListener('click', clickEvent);
+  squareFive.addEventListener('click', clickEvent);
+  squareSix.addEventListener('click', clickEvent);
+  squareSeven.addEventListener('click', clickEvent);
+  squareEight.addEventListener('click', clickEvent);
+  squareNine.addEventListener('click', clickEvent);
 
 // end of 'DOMContentLoaded'
 });
