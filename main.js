644b6dce1c7aed5cd924a002eae9bcf2503e621a
mikesmall <michael.l.a.small@gmail.com>
2017-07-14 17:39:51 -0400

    Added event listener for 'New Game' button but I don't think it's doing anything

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  var newGame = document.querySelector('.new_game');
+  newGame.addEventListener('click', function() {
+    console.log('The button was clicked');
+    location.reload();
+  });
 
-
-// end of 'DOMContentLoaded' 
+// end of 'DOMContentLoaded'
 });
