0b0141a8d0608439f4c4f3407c4de3428415332c
mikesmall <michael.l.a.small@gmail.com>
2017-07-15 17:09:40 -0400

    Winning combos converted to array of arrays

--- main.js
+++ main.js
@@ -18,15 +18,16 @@ document.addEventListener('DOMContentLoaded', function() {
   var squareEight = document.querySelector('.eight');
   var squareNine = document.querySelector('.nine');
 
-  // List of winning square-combinations::
-  // 1, 2, 3
-  // 4, 5, 6
-  // 7, 8, 9
-  // 1, 4, 7
-  // 2, 5, 8
-  // 3, 6, 9
-  // 1, 5, 9
-  // 3, 5, 7
+  var winningCombos = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+  ];
 
 
   // Player X goes first!
