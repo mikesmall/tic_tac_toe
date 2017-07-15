f0f01e4a475071e63a9c2ec011f87f3dbcdb6121
mikesmall <michael.l.a.small@gmail.com>
2017-07-15 15:35:44 -0400

    Game now alternates between X and O clicks, while (brokenly) displaying whose turn it is

--- main.js
+++ main.js
@@ -18,85 +18,132 @@ document.addEventListener('DOMContentLoaded', function() {
   var squareEight = document.querySelector('.eight');
   var squareNine = document.querySelector('.nine');
 
+  // Player X goes first!
+  var whoseTurn = 'X';
+
+  // Show whose turn it is:
+  var displayPlayer = document.querySelector('.display_player');
+  displayPlayer.innerText = whoseTurn;
+
   // Event listeners for each square:
 
   var clickOne = squareOne.addEventListener('click', function() {
     console.log('Square 1 was clicked');
     this.innerHTML = '<span class="xo">' + whoseTurn + '</span>';
+    if (whoseTurn === 'X') {
+      whoseTurn = 'O'
+    } else if (whoseTurn === 'O') {
+      whoseTurn = 'X'
+    }
   });
 
   var clickTwo = squareTwo.addEventListener('click', function() {
     console.log('Square 2 was clicked');
     this.innerHTML = '<span class="xo">' + whoseTurn + '</span>';
+    if (whoseTurn === 'X') {
+      whoseTurn = 'O'
+    } else if (whoseTurn === 'O') {
+      whoseTurn = 'X'
+    }
   });
 
   var clickThree = squareThree.addEventListener('click', function() {
     console.log('Square 3 was clicked');
     this.innerHTML = '<span class="xo">' + whoseTurn + '</span>';
+    if (whoseTurn === 'X') {
+      whoseTurn = 'O'
+    } else if (whoseTurn === 'O') {
+      whoseTurn = 'X'
+    }
   });
 
   var clickFour = squareFour.addEventListener('click', function() {
     console.log('Square 4 was clicked');
     this.innerHTML = '<span class="xo">' + whoseTurn + '</span>';
+    if (whoseTurn === 'X') {
+      whoseTurn = 'O'
+    } else if (whoseTurn === 'O') {
+      whoseTurn = 'X'
+    }
   });
 
   var clickFive = squareFive.addEventListener('click', function() {
     console.log('Square 5 was clicked');
     this.innerHTML = '<span class="xo">' + whoseTurn + '</span>';
+    if (whoseTurn === 'X') {
+      whoseTurn = 'O'
+    } else if (whoseTurn === 'O') {
+      whoseTurn = 'X'
+    }
   });
 
   var clickSix = squareSix.addEventListener('click', function() {
     console.log('Square 6 was clicked');
     this.innerHTML = '<span class="xo">' + whoseTurn + '</span>';
+    if (whoseTurn === 'X') {
+      whoseTurn = 'O'
+    } else if (whoseTurn === 'O') {
+      whoseTurn = 'X'
+    }
   });
 
   var clickSeven = squareSeven.addEventListener('click', function() {
     console.log('Square 7 was clicked');
     this.innerHTML = '<span class="xo">' + whoseTurn + '</span>';
+    if (whoseTurn === 'X') {
+      whoseTurn = 'O'
+    } else if (whoseTurn === 'O') {
+      whoseTurn = 'X'
+    }
   });
 
   var clickEight = squareEight.addEventListener('click', function() {
     console.log('Square 8 was clicked');
     this.innerHTML = '<span class="xo">' + whoseTurn + '</span>';
+    if (whoseTurn === 'X') {
+      whoseTurn = 'O'
+    } else if (whoseTurn === 'O') {
+      whoseTurn = 'X'
+    }
   });
 
   var clickNine = squareNine.addEventListener('click', function() {
     console.log('Square 9 was clicked');
     this.innerHTML = '<span class="xo">' + whoseTurn + '</span>';
+    if (whoseTurn === 'X') {
+      whoseTurn = 'O'
+    } else if (whoseTurn === 'O') {
+      whoseTurn = 'X'
+    }
   });
 
-  // Determine whose turn it is (X or O)
-  var currentPlayer = document.querySelector('.current_player');
-  var whoseTurn = 'X'
 
-  if (whoseTurn === 'X') {
-    this.innerHTML = '<span class="xo">X</span>'
-  } else if (whoseTurn === 'O') {
-    this.innerHTML = '<span class="xo">O</span>'
-  }
-  currentPlayer.innerText = whoseTurn;
 
-  // Display the turn-name (X/O)
-  // -- Need a spot in the markup for this
+
+  //
+  // if (whoseTurn === 'X') {
+  //   this.innerHTML = '<span class="xo">X</span>'
+  // } else if (whoseTurn === 'O') {
+  //   this.innerHTML = '<span class="xo">O</span>'
+  // }
+
   // onClick (square):
   // -- check if square's occupied
   // ----- if so, do nothing
   // ----- if not, put name-of-turn in square
-
-
 
   // ----- check if a 'winning row' has been created
   // -------- if yes, game over
   // -------- if no, switch player, restart 'turn' function
 
 
-  // jQuery to make squares change colour on click:
+  // A jQuery alternative to make squares change colour on click:
+
   // $('.square').click(function(){
   //   console.log('jQuery click alert');
   //   $(this).css({"background-color": "salmon" });
   //   this.innerHTML = '<span class="xo">Hi</span>';
   // })
-
 
 // end of 'DOMContentLoaded'
 });
